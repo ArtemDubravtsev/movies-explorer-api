2496bf3cfc1098709d2b453992b765ef71de4f3a
Artem Dubravtsev <a.dubravtsev@yandex.ru>
2024-01-05 16:06:37 +0200

    правки CORS

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ const app = express();
 
 const corsOptions = {
   origin: 'http://localhost:3000',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
   credentials: true,
 };
 
